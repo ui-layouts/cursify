6ceb9e6ad59be6430298feb0995f23e31caedae1
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-11-27 00:17:25 +0530

    add vercl analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from './providers/theme-provider.tsx';
-
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <HelmetProvider>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
           <App />
+          <Analytics />
         </ThemeProvider>
       </HelmetProvider>
     </BrowserRouter>
